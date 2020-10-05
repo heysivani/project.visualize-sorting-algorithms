edf55b87d9a873f6d7b8c10ae9d522f86066cf2b
heysivani <heysivani@gmail.com>
2020-10-05 16:24:58 +0900

    added tests for sort

--- test/_sort.js
+++ test/_sort.js
@@ -8,4 +8,15 @@ describe("Sort", () => {
   it("should have a sort method", () => {
     expect(Sort.prototype.sort).to.be.a("function");
   });
+  it("should have a merge method", () => {
+    expect(Sort.prototype.merge).to.be.a("function");
+  });
+  it("should return correctly sorted elements", () => {
+    let input = [3, 11, 1, 2, 7, 5, 9];
+    let mergeSort = new Sort(input);
+    let expected = [1, 2, 3, 5, 7, 9, 11];
+    let actual = mergeSort.sort();
+
+    expect(actual).to.equal(expected);
+  });
 });
